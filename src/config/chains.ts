9afeed6beb4fb556d06628c411745a3be6e68eb7
Vu Viet Tai <vuviettai@gmail.com>
2024-09-13 16:24:31 +0700

    Add config for local env

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,21 +1,31 @@
+// Testnet
 import testnetAxelar from '../../data/testnet/axelar.json';
 import testnetCosmos from '../../data/testnet/cosmos.json';
 import testnetEvm from '../../data/testnet/evm.json';
 import testnetBtc from '../../data/testnet/btc.json';
 import testnetRabbitmq from '../../data/testnet/rabbitmq.json';
+// Devnet
 import devnetAxelar from '../../data/devnet/axelar.json';
 import devnetCosmos from '../../data/devnet/cosmos.json';
 import devnetEvm from '../../data/devnet/evm.json';
 import devnetBtc from '../../data/devnet/btc.json';
 import devnetRabbitmq from '../../data/devnet/rabbitmq.json';
+// Local
+import localAxelar from '../../data/local/axelar.json';
+import localCosmos from '../../data/local/cosmos.json';
+import localEvm from '../../data/local/evm.json';
+import localBtc from '../../data/local/btc.json';
+import localRabbitmq from '../../data/local/rabbitmq.json';
+
+
 import { env } from '.';
 import { BtcNetworkConfig, CosmosNetworkConfig, EvmNetworkConfig, RabbitMQConfig } from './types';
 
-const cosmos = env.CHAIN_ENV === 'devnet' ? devnetCosmos : testnetCosmos;
-const axelar = env.CHAIN_ENV === 'devnet' ? devnetAxelar : testnetAxelar;
-const evm = env.CHAIN_ENV === 'devnet' ? devnetEvm : testnetEvm;
-const btc = env.CHAIN_ENV === 'devnet' ? devnetBtc : testnetBtc;
-const rabbitmq = env.CHAIN_ENV === 'devnet' ? devnetRabbitmq : testnetRabbitmq;
+const cosmos = env.CHAIN_ENV === 'local' ? localCosmos :(env.CHAIN_ENV === 'devnet' ? devnetCosmos : testnetCosmos);
+const axelar = env.CHAIN_ENV === 'local' ? localAxelar : (env.CHAIN_ENV === 'devnet' ? devnetAxelar : testnetAxelar);
+const evm = env.CHAIN_ENV === 'local' ? localEvm : (env.CHAIN_ENV === 'devnet' ? devnetEvm : testnetEvm);
+const btc = env.CHAIN_ENV === 'local' ? localBtc : (env.CHAIN_ENV === 'devnet' ? devnetBtc : testnetBtc);
+const rabbitmq = env.CHAIN_ENV === 'local' ? localRabbitmq : (env.CHAIN_ENV === 'devnet' ? devnetRabbitmq : testnetRabbitmq);
 export const cosmosChains: CosmosNetworkConfig[] = cosmos.map((chain) => ({
   ...chain,
   mnemonic: env.AXELAR_MNEMONIC,
