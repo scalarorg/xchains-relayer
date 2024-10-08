26b64352368633c303cd341b306b3c750cd27e02
David Tran <tranhuyducseven@gmail.com>
2024-10-08 10:01:54 +0700

    feat: add log config

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,19 @@
-import { logger } from './logger';
+import { axelarChain, btcChains, cosmosChains, evmChains, rabbitmqConfigs } from 'config';
 import { startAPIServer } from './api';
+import { logger } from './logger';
 import { startRelayer } from './relayer';
 logger.info('Starting relayer api server...');
+
+const logConfig = () => {
+  console.log({
+    btcChains,
+    evmChains,
+    axelarChain,
+    rabbitmqConfigs,
+    cosmosChains,
+  });
+};
+
+logConfig();
 startAPIServer();
 startRelayer();
